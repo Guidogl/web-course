9a38e6d11b94f5f6561dc6f5157cc3ddd4742946
Gvidas <gvidas@skydrop.me>
2023-09-13 11:17:42 +0300

    Landing Completed

--- script.js
+++ script.js
@@ -14,9 +14,76 @@ const getArticles = async () => {
 }
 
 async function startApplication() {
-    const articles = await getArticles();
+    const articlesFetchResult = await getArticles();
 
-    console.log(articles);
+    console.log(articlesFetchResult);
+
+    const latestNews = document.getElementById('articles-latest');
+
+    articlesFetchResult.articles
+        .filter((article) => article.category === 'Technology')
+        .forEach((article) => {
+            const articleElement = document.createElement('div');
+            articleElement.classList.add('article-latest');
+
+            const articleImage = document.createElement('img');
+            // articleImage.classList.add('article-latest-image');
+            articleImage.src = article.image;
+
+            const articleMainDetails = document.createElement('div');
+            articleMainDetails.classList.add('article-latest-main');
+
+            const articleTitle = document.createElement('h3');
+            articleTitle.textContent = article.title;
+
+            const articleDate = document.createElement('h3');
+            articleDate.textContent = article.date;
+
+            articleMainDetails.appendChild(articleTitle);
+            articleMainDetails.appendChild(articleDate);
+
+            const articleText = document.createElement('p');
+            articleText.textContent = article.description;
+
+            articleElement.appendChild(articleImage);
+            articleElement.appendChild(articleMainDetails);
+            // articleElement.appendChild(articleDate);
+            articleElement.appendChild(articleText);
+
+            latestNews.appendChild(articleElement);
+        });
+
+
+    const breakingNews = document.getElementById('articles-breaking');
+
+    articlesFetchResult.articles
+        .filter((article) => article.category === 'News')
+        .forEach((article) => {
+            const articleElement = document.createElement('div');
+            articleElement.classList.add('article-breaking');
+
+            const articleImage = document.createElement('img');
+            articleImage.classList.add('article-breaking-image');
+            articleImage.src = article.image;
+
+            const articleMainDetails = document.createElement('div');
+            articleMainDetails.classList.add('article-breaking-main');
+
+            const articleTitle = document.createElement('h3');
+            articleTitle.textContent = article.title;
+
+            const articleDate = document.createElement('h3');
+            articleDate.textContent = article.date;
+
+            articleMainDetails.appendChild(articleTitle);
+            articleMainDetails.appendChild(articleDate);
+
+            articleElement.appendChild(articleImage);
+            articleElement.appendChild(articleMainDetails);
+            // articleElement.appendChild(articleDate);
+
+            breakingNews.appendChild(articleElement);
+        });
 
     // const greetingElement = document.getElementById('greeting');
 
